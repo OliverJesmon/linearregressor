51fd4bfc83e2ab76bf8f88fdc5e26327512edc7d
OliverJesmon <102456180+OliverJesmon@users.noreply.github.com>
2023-11-09 22:12:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
-import matplotlib.pyplot as pl
-import numpy as np 
-from sklearn import linear_model
-x_train=np.array([[1],[2],[3],[4]])
+import matplotlib.pyplot as pl #ploting library
+import numpy as np #to work with array
+from sklearn import linear_model #to import regressor
+x_train=np.array([[1],[2],[3],[4]])#training data
 y_train=np.array([[26],[25],[25],[24]])
-x_test=np.array([[5]])
+x_test=np.array([[5]])#feature by user
 
 model=linear_model.LinearRegression()
-model.fit(x_train,y_train)
-y_predict=model.predict(x_test)
+model.fit(x_train,y_train) #getting ready with ml model
+y_predict=model.predict(x_test)#label or o/p by machine 
 print("Coefficient(mx):",model.coef_)
-print("intercept(b)",model.intercept_)
+print("intercept(b)",model.intercept_)#y=mx+b
 
 pl.plot(x_test,y_predict,'o')
 pl.show()
+#by Oliver Jesmon
